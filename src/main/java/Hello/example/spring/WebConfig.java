7040cca19634262dafc15e281ba91138f0939b5d
enaa99 <wprkfdlsgk@gmail.com>
2024-02-20 23:53:36 +0900

    feat: add spring Interceptor logInterceptor

--- src/main/java/Hello/example/spring/WebConfig.java
+++ src/main/java/Hello/example/spring/WebConfig.java
@@ -1,13 +1,25 @@
 package Hello.example.spring;
 
 import Hello.example.spring.Filter.LogFilter;
+import Hello.example.spring.interceptor.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/error");
+
+    }
 
     @Bean
     public FilterRegistrationBean logFilter() {
@@ -15,5 +27,6 @@ public class WebConfig {
         filterFilterRegistrationBean.setFilter(new LogFilter());
         filterFilterRegistrationBean.setOrder(1);
         filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
     }
 }
